2b1489f2315c20ffc7382e74ead0cab8d1ad6402
Muhammed <muhammed@sendme.ng>
2023-11-04 19:43:03 +0100

    feat: implement error handling and rate limiting via throttling

--- data-ingestion/src/ingestion.py
+++ data-ingestion/src/ingestion.py
@@ -3,13 +3,16 @@ import os
 #https://pypi.org/project/websocket_client/
 import websocket
 import json
+import time
+
+# Define the rate limit (requests per second)
+rate_limit = os.getenv("RATE_LIMIT")
 
 load_dotenv()
 token = os.getenv("FINNHUB_TOKEN")
 
 class Producer:
     def __init__(self):
-        print(token)
         websocket.enableTrace(True)
         self.ws = websocket.WebSocketApp(
             f"wss://ws.finnhub.io?token={token}",
@@ -18,7 +21,10 @@ class Producer:
             on_close = self.on_close
         )
         self.ws.on_open = self.on_open
-        self.ws.run_forever()
+        try:
+            self.ws.run_forever()
+        except KeyboardInterrupt:
+            self.ws.close()
 
     def on_message(self, ws, message):
         print(message)
@@ -26,14 +32,21 @@ class Producer:
     def on_error(self, ws, error):
         print(error)
 
-    def on_close(self, ws):
-        print("### closed ###")
+    def on_close(self, ws, close_status_code, close_msg):
+        print(f"Websocket closed with status code {close_status_code}: {close_msg}")
 
     def on_open(self, ws):
-        ws.send('{"type":"subscribe","symbol":"AAPL"}')
-        ws.send('{"type":"subscribe","symbol":"AMZN"}')
-        ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
-        ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
+        while True:
+            self.send_messages(ws)
+            time.sleep(1 / rate_limit)
+
+    def send_messages(self, ws):
+        try:
+            ws.send('{"type":"subscribe","symbol":"AAPL"}')
+            ws.send('{"type":"subscribe","symbol":"AMZN"}')
+            ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+        except Exception as e:
+            print(f"Failed to send data via Websocket: {e}")
 
 if __name__ == "__main__":
     Producer()
